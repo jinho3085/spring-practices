f41adeb9c5f7b1d3eb82b9c0cca72bc6e7aed7a9
jinho3085 <jinho3085@naver.com>
2025-09-18 09:49:31 +0900

    cd,dvd

---
+++ bean-wiring/src/test/java/config/soundsystem/CDPlayerXmlConfigTest.java
@@ -0,0 +1,28 @@
+package config.soundsystem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import soundsystem.CDPlayer;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations={"classpath:config/soundsystem/applicationContext.xml"})
+public class CDPlayerXmlConfigTest {
+	@Autowired
+	CDPlayer cdPlayer;
+	
+	@Test
+	public void testCDPlayerNotNull() {
+		assertNotNull(cdPlayer);
+	}
+	
+	public void testPlay() {
+		assertEquals("playing 붕붕 by 김하온", cdPlayer.play());
+	}
+}
